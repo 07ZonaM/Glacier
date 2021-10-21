23da7fe93ac9e24f603a9b8af944202d373d0552
07ZonaM <92954521+07ZonaM@users.noreply.github.com>
2021-10-21 23:47:22 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/07ZonaM/Glacier.git
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='Glacier',
+    version='0.1.0',
+    description='Glacier Mapping',
+    author='Various',
+    author_email='test@test.com',
+    url='https://github.com/07ZonaM/Glacier',
+    install_requires=required,
+    packages=find_packages(exclude=('web', 'ee_code', 'docs', 'conf', 'cluster'))
+)
+
+
